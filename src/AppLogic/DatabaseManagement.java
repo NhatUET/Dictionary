17d6b669521de8f565dbb0372b1035c1690502b0
nhat123-dev <minhnhat8dc@gmail.com>
2021-12-09 10:37:47 +0700

    first commit

---
+++ src/AppLogic/DatabaseManagement.java
@@ -0,0 +1,169 @@
+package AppLogic;
+
+import java.sql.*;
+
+public class DatabaseManagement {
+
+    public static Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:res/defaul/data.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static Connection connectForUser() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:res/modify/data.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static boolean checkWord(String word) {
+        String sqlSearch = "SELECT * FROM av WHERE word LIKE ?";
+
+        try {
+            Connection conn = connectForUser();
+
+            PreparedStatement ps  = conn.prepareStatement(sqlSearch);
+            ps.setString(1, word);
+            ResultSet rs = ps.executeQuery();
+            String res = "";
+
+            while (rs.next()) {
+                String meaning = rs.getString("word");
+                res = meaning;
+            }
+            if (res.isEmpty()) return false;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return true;
+    }
+    public void dictionarySearcherList(String keyWord){
+        String sqlSearch = "SELECT word FROM av WHERE word LIKE ?";
+
+        try {
+            Connection conn = connect();
+
+            PreparedStatement ps  = conn.prepareStatement(sqlSearch);
+            ps.setString(1,keyWord + "%");
+            ResultSet rs    = ps.executeQuery();
+
+
+            while (rs.next()) {
+                String meaning = rs.getString("mean");
+
+                System.out.println(rs.getString("word"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public void dictionarySearcher(String word){
+        String sqlSearch = "SELECT * FROM av WHERE word LIKE ?";
+
+        try {
+            Connection conn = connectForUser();
+
+            PreparedStatement ps  = conn.prepareStatement(sqlSearch);
+            ps.setString(1, word);
+            ResultSet rs = ps.executeQuery();
+
+
+            while (rs.next()) {
+                String meaning = rs.getString("mean");
+
+                System.out.println(rs.getString("word") +  "\r\n\t" +
+                        rs.getString("pro") + "\t" + rs.getString("mean"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void insertWord(String word, String pro, String mean) {
+        String sqlInsert = "INSERT INTO av (word, pro, mean) VALUES (?, ?, ?)";
+
+        try {
+            Connection conn = connectForUser();
+            PreparedStatement ps  = conn.prepareStatement(sqlInsert);
+            ps.setString(1,word);
+            ps.setString(2,pro);
+            ps.setString(3,mean);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public static void removeWord(String word) {
+        String sqlRemove = "DELETE FROM av WHERE word = ?";
+
+        try {
+            Connection conn = connectForUser();
+            PreparedStatement ps  = conn.prepareStatement(sqlRemove);
+
+            ps.setString(1,word);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     *
+     * @param word Sửa từ nào.
+     * @param pro Sửa cai gi.
+     * @param mean Su cai gi.
+     */
+    public static void editWord(String word, String pro, String mean) {
+        //Vì cái db set là NOT NULL nên
+        //Khi chuỗi edit nào rỗng nó vi phạm ràng buộc NOT NULL của các cột
+        //SQLite sẽ khôi phục -> không thực  hiện
+
+        String sqlEdit = "REPLACE INTO av (word, pro, mean) VALUES(?, ?, ?)";
+
+        try {
+            Connection conn = connectForUser();
+            PreparedStatement ps  = conn.prepareStatement(sqlEdit);
+
+            ps.setString(1,word);
+            ps.setString(2,pro);
+            ps.setString(3,mean);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DatabaseManagement app = new DatabaseManagement();
+        app.dictionarySearcher("cat");
+        app.editWord("cat", "woof", "yeu");
+        app.insertWord("cattt", "meow", "ghet cho");
+        app.dictionarySearcher("cat");
+        app.dictionarySearcher("cattt");
+        if (checkWord("dangnhat")) System.out.println("co");
+        else System.out.println("ko");
+    }
+}
---
+++ src/AppLogic/Translator.java
@@ -0,0 +1,55 @@
+package AppLogic;
+
+import java.util.Locale;  
+import javax.speech.Central;  
+import javax.speech.synthesis.Synthesizer;  
+import javax.speech.synthesis.SynthesizerModeDesc;  
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
+
+public class Translator {
+    
+    public static String translate(String langFrom, String langTo, String text) throws IOException {
+        // INSERT YOU URL HERE
+        String urlStr = "https://script.google.com/macros/s/AKfycbygFnyIKNVv0GhUCQSJ3NTUG-RCHFVteZM3im5coGQfVpx5adc/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+    
+    public static void speech(String text) throws IOException {
+        try {  
+            //setting properties as Kevin Dictionary  
+            System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us" + ".cmu_us_kal.KevinVoiceDirectory");  
+            //registering speech engine  
+            Central.registerEngineCentral("com.sun.speech.freetts" + ".jsapi.FreeTTSEngineCentral");  
+            //create a Synthesizer that generates voice  
+            Synthesizer synthesizer = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));  
+            //allocates a synthesizer  
+            synthesizer.allocate();  
+            //resume a Synthesizer  
+            synthesizer.resume();  
+            //speak the specified text until the QUEUE become empty  
+            synthesizer.speakPlainText(text, null);  
+            synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+    }
+}
